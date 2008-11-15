bb5acfaa04f4e6095a2e9792f9b494f6fb1585d6
Dan Dudley <born2snipe@gmail.com>
2008-11-14 18:22:11 -0600

    cleaned up abstract record layout

--- commonline-core/src/main/java/commonline/core/layout/AbstractCommonLineRecordLayout.java
+++ commonline-core/src/main/java/commonline/core/layout/AbstractCommonLineRecordLayout.java
@@ -1,17 +1,11 @@
 package commonline.core.layout;
 
-import flapjack.layout.FieldDefinition;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
+import flapjack.layout.SimpleRecordLayout;
 
 /**
  * An abstract class simplifying the construction of the CommonLineFieldDefinitions for a CommonLineRecordLayout
  */
-public abstract class AbstractCommonLineRecordLayout implements CommonLineRecordLayout {
-    private List fieldDefinitions = new ArrayList();
+public abstract class AbstractCommonLineRecordLayout extends SimpleRecordLayout implements CommonLineRecordLayout {
     private int offset = 0;
 
     protected AbstractCommonLineRecordLayout() {
@@ -31,7 +25,7 @@ public abstract class AbstractCommonLineRecordLayout implements CommonLineRecord
      * @param length - the length in bytes of this field
      */
     protected void filler(int length) {
-        addMiddleField(new FillerFieldDefinition(getNextPosition(), length), true);
+        addMiddleField(new FillerFieldDefinition(offset, length));
     }
 
 
@@ -42,7 +36,7 @@ public abstract class AbstractCommonLineRecordLayout implements CommonLineRecord
      * @param length - the length in bytes of this field
      */
     protected void text(String name, int length) {
-        addMiddleField(new TextFieldDefinition(getNextPosition(), length, name), true);
+        addMiddleField(new TextFieldDefinition(offset, length, name));
     }
 
     /**
@@ -52,7 +46,7 @@ public abstract class AbstractCommonLineRecordLayout implements CommonLineRecord
      * @param length - the length in bytes of this field
      */
     protected void number(String name, int length) {
-        addMiddleField(new NumberFieldDefinition(getNextPosition(), length, name), true);
+        addMiddleField(new NumberFieldDefinition(offset, length, name));
     }
 
     /**
@@ -62,7 +56,7 @@ public abstract class AbstractCommonLineRecordLayout implements CommonLineRecord
      * @param length - the length in bytes of this field
      */
     protected void fixedNumber(String name, int length) {
-        addMiddleField(new FixedNumberFieldDefinition(getNextPosition(), length, name), true);
+        addMiddleField(new FixedNumberFieldDefinition(offset, length, name));
     }
 
     /**
@@ -72,62 +66,20 @@ public abstract class AbstractCommonLineRecordLayout implements CommonLineRecord
      * @param length - the length in bytes of this field
      */
     protected void fixedText(String name, int length) {
-        addMiddleField(new FixedTextFieldDefinition(getNextPosition(), length, name), true);
-    }
-
-    private int getNextPosition() {
-        return offset;
-    }
-
-    private void removeFieldDefinition(Class fieldDefinitionClass) {
-        FieldDefinition match = null;
-        for (int i = fieldDefinitions.size() - 1; i >= 0; i--) {
-            FieldDefinition def = (FieldDefinition) fieldDefinitions.get(i);
-            if (fieldDefinitionClass.isInstance(def)) {
-                match = def;
-                break;
-            }
-        }
-
-        if (match != null) {
-            fieldDefinitions.remove(match);
-        }
+        addMiddleField(new FixedTextFieldDefinition(offset, length, name));
     }
 
     private void recordCodeField() {
-        addMiddleField(new RecordCodeFieldDefinition(), false);
+        addMiddleField(new RecordCodeFieldDefinition());
     }
 
-    private void addMiddleField(CommonLineFieldDefinition fieldDefinition, boolean moveTrailingFields) {
-//        if (moveTrailingFields) {
-//            removeTrailingFields();
-//        }
-        fieldDefinitions.add(fieldDefinition);
+    private void addMiddleField(CommonLineFieldDefinition fieldDefinition) {
+        super.addFieldDefinition(fieldDefinition);
         offset += fieldDefinition.getLength();
-//        if (moveTrailingFields) {
-//            addTrailingFields();
-//        }
     }
 
     private void addTrailingFields() {
-        fieldDefinitions.add(new RecordTerminatorFieldDefinition(offset));
+        super.addFieldDefinition(new RecordTerminatorFieldDefinition(offset));
     }
 
-    private void removeTrailingFields() {
-        removeFieldDefinition(RecordTerminatorFieldDefinition.class);
-    }
-
-    public List getFieldDefinitions() {
-        return Collections.unmodifiableList(fieldDefinitions);
-    }
-
-    public int getLength() {
-        int length = 0;
-        Iterator it = fieldDefinitions.iterator();
-        while (it.hasNext()) {
-            FieldDefinition def = (FieldDefinition) it.next();
-            length += def.getLength();
-        }
-        return length;
-    }
 }
