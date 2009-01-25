5abf3a46ed06583fcf3077cb8175fc62014a23ca
Dan Dudley <born2snipe@gmail.com>
2009-01-24 22:00:22 -0600

    created release profile

--- commonline-viewer/src/main/java/commonline/gui/App.java
+++ commonline-viewer/src/main/java/commonline/gui/App.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2008-2009 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at:
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is
  * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and limitations under the License.
@@ -44,7 +44,8 @@ public class App extends JFrame {
         open.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 File fileToOpen = loadFile(App.this);
-                tabs.addTab(fileToOpen.getName(), new FilePanel());
+                if (fileToOpen != null)
+                    tabs.addTab(fileToOpen.getName(), new FilePanel());
             }
         });
 
@@ -78,6 +79,8 @@ public class App extends JFrame {
         FileDialog fd = new FileDialog(f, "Open...", FileDialog.LOAD);
         fd.setLocationRelativeTo(f);
         fd.show();
+        if (fd.getFile() == null)
+            return null;
         return new File(fd.getFile());
     }
 
--- commonline-viewer/src/main/java/commonline/model/CommonLineRecord.java
+++ commonline-viewer/src/main/java/commonline/model/CommonLineRecord.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2008-2009 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at:
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is
  * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and limitations under the License.
@@ -16,15 +16,23 @@ import commonline.core.layout.CommonLineRecordLayout;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Collections;
 
 
 public class CommonLineRecord {
     private Map<String, byte[]> fields = new LinkedHashMap<String, byte[]>();
     private CommonLineRecordLayout recordLayout;
 
-
     public CommonLineRecord(Map<String, byte[]> fields, CommonLineRecordLayout recordLayout) {
         this.fields = fields;
         this.recordLayout = recordLayout;
+    }
+
+    public Map<String, byte[]> getFields() {
+        return Collections.unmodifiableMap(fields);
+    }
+
+    public CommonLineRecordLayout getRecordLayout() {
+        return recordLayout;
     }
 }
