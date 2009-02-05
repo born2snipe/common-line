281967fa2bc68cee18e71ca6ef4debf57fe54c7b
Dan Dudley <born2snipe@gmail.com>
2009-02-04 22:53:45 -0600

    fixed to handle if not enough data is given

--- commonline4/src/test/java/commonline/cl4/changesend/parser/ChangeSendRecordLayoutResolverTest.java
+++ commonline4/src/test/java/commonline/cl4/changesend/parser/ChangeSendRecordLayoutResolverTest.java
@@ -1,11 +1,11 @@
 /**
  * Copyright 2008-2009 the original author or authors.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at:
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is
  * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and limitations under the License.
@@ -14,11 +14,10 @@ package commonline.cl4.changesend.parser;
 
 import commonline.cl4.changesend.layout.BorrowerDetailRecordLayout;
 import commonline.cl4.changesend.layout.HeaderRecordLayout;
+import flapjack.layout.RecordLayout;
 import junit.framework.TestCase;
 
 import java.util.Arrays;
-
-import flapjack.layout.RecordLayout;
 
 public class ChangeSendRecordLayoutResolverTest extends TestCase {
     private ChangeSendRecordLayoutResolver resolver;
@@ -42,8 +41,12 @@ public class ChangeSendRecordLayoutResolverTest extends TestCase {
         assertTrue(recordLayout instanceof BorrowerDetailRecordLayout);
     }
 
-    public void test_resolve_NotEnoughDataGiven() {
+    public void test_resolve_NotDataGiven() {
         assertNull(resolver.resolve(new byte[0]));
+    }
+
+    public void test_resolve_OneByteGiven() {
+        assertNull(resolver.resolve(new byte[1]));
     }
 
 }
